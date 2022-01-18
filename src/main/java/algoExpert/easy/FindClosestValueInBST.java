c92563f2cf5adbba69111e9023725558d65944c7
alireza_bayat <a.bayat1376@yahoo.com>
2022-01-18 12:46:55 +0330

    find closest value in bst

---
+++ src/main/java/algoExpert/easy/FindClosestValueInBST.java
@@ -0,0 +1,59 @@
+package algoExpert.easy;
+
+import algoExpert.utils.BinaryTree;
+
+/**
+ * @author alireza_bayat
+ * created on 1/18/22
+ */
+public class FindClosestValueInBST {
+
+    // time complexity O(log(n)) | space complexity O(log(n)) cause of stack call back and frame passes
+    //worst case time O(n) in un-balanced tree
+    public int findClosestValueInBSTRecursive(BinaryTree binaryTree, int target) {
+        //root node value would be the closest for the starting point
+        return findClosestValueInBSTHelperRecursive(binaryTree, target, binaryTree.getValue());
+    }
+
+    // time complexity O(log(n)) | space complexity O(1)
+    //worst case time O(n) in un-balanced tree
+    public int findClosestValueInBSTIterative(BinaryTree binaryTree, int target) {
+        return findClosestValueInBSTHelperIterative(binaryTree, target, binaryTree.getValue());
+    }
+
+    private int findClosestValueInBSTHelperRecursive(BinaryTree tree, int target, int closest) {
+        if (tree == null)
+            return closest;
+
+        int currentNodeClosestDifference = Math.abs(closest - target);
+        int closestDifference = Math.abs(tree.getValue() - target);
+        if (closestDifference < currentNodeClosestDifference)
+            closest = tree.getValue();
+
+        if (target > tree.getValue())
+            return findClosestValueInBSTHelperRecursive(tree.getRight(), target, closest);
+        else if (target < tree.getValue())
+            return findClosestValueInBSTHelperRecursive(tree.getLeft(), target, closest);
+        else
+            return closest;
+    }
+
+    public int findClosestValueInBSTHelperIterative(BinaryTree tree, int target, int closest) {
+        while (tree != null) {
+            int currentNodeClosestDifference = Math.abs(closest - target);
+            int closestDifference = Math.abs(tree.getValue() - target);
+            if (closestDifference < currentNodeClosestDifference)
+                closest = tree.getValue();
+
+            if (target > tree.getValue())
+                tree = tree.getRight();
+            else if (target < tree.getValue())
+                tree = tree.getLeft();
+            else {
+                closest = tree.getValue();
+                break;
+            }
+        }
+        return closest;
+    }
+}
---
+++ src/test/java/algoExpert/easy/FindClosestValueInBSTTest.java
@@ -0,0 +1,52 @@
+package algoExpert.easy;
+
+import algoExpert.utils.BinaryTree;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FindClosestValueInBSTTest {
+
+
+    BinaryTree rootNodeBST;
+    FindClosestValueInBST findClosestValueInBST;
+
+    @Before
+    public void setup() {
+        findClosestValueInBST = new FindClosestValueInBST();
+        rootNodeBST = new BinaryTree(10);
+        BinaryTree root_left = new BinaryTree(8);
+        BinaryTree root_right = new BinaryTree(15);
+        BinaryTree root_left_left = new BinaryTree(3);
+        BinaryTree root_left_right = new BinaryTree(6);
+        BinaryTree root_right_left = new BinaryTree(11);
+        BinaryTree root_right_right = new BinaryTree(16);
+        BinaryTree root_right_right_right = new BinaryTree(20);
+
+        root_right_right.setRight(root_right_right_right);
+        root_right.setLeft(root_right_left);
+        root_right.setRight(root_right_right);
+        root_left.setRight(root_left_right);
+        root_left.setLeft(root_left_left);
+        rootNodeBST.setLeft(root_left);
+        rootNodeBST.setRight(root_right);
+    }
+
+    @Test
+    public void findClosestValueInBST() {
+        Assert.assertEquals(20, findClosestValueInBST.findClosestValueInBSTRecursive(rootNodeBST, 21));
+        Assert.assertEquals(20, findClosestValueInBST.findClosestValueInBSTIterative(rootNodeBST, 21));
+    }
+
+    @Test
+    public void findClosestValueInBST_lessThanRoot() {
+        Assert.assertEquals(3, findClosestValueInBST.findClosestValueInBSTRecursive(rootNodeBST, 2));
+        Assert.assertEquals(3, findClosestValueInBST.findClosestValueInBSTIterative(rootNodeBST, 2));
+    }
+
+    @Test
+    public void findClosestValueInBST_equalToRoot() {
+        Assert.assertEquals(11, findClosestValueInBST.findClosestValueInBSTRecursive(rootNodeBST, 11));
+        Assert.assertEquals(11, findClosestValueInBST.findClosestValueInBSTIterative(rootNodeBST, 11));
+    }
+}
